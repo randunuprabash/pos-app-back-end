473cfa022e73bfe8ae1cc38a5525c5233a13a5ab
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-09-14 17:04:22 +0530

    Revamp the GET

--- src/main/java/lk/ijse/dep7/servletposapp/api/CustomerServlet.java
+++ src/main/java/lk/ijse/dep7/servletposapp/api/CustomerServlet.java
@@ -2,7 +2,6 @@ package lk.ijse.dep7.servletposapp.api;
 
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
-import jakarta.json.bind.JsonbConfig;
 import jakarta.json.bind.JsonbException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -14,22 +13,36 @@ import lk.ijse.dep7.servletposapp.util.DBConnection;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(urlPatterns = "/customers")
 public class CustomerServlet extends HttpServlet {
 
-    private Jsonb jsonb = JsonbBuilder.create();
+    private final Jsonb jsonb = JsonbBuilder.create();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        /* GET http://localhost:8080/pos/customers          - All Customers */
+        /* GET http://localhost:8080/pos/customers?id=C001  - C001 Customer */
+        /* GET http://localhost:8080/pos/customers?id=C100  - 404 */
+
         try (Connection connection = DBConnection.getConnection()) {
 
-            Statement stm = connection.createStatement();
-            ResultSet rst = stm.executeQuery("SELECT * FROM customer");
+            String id = req.getParameter("id");
+            String sql = (id == null) ? "SELECT * FROM customer" : "SELECT * FROM customer WHERE id=?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+
+            if (id != null) {
+                stm.setString(1, id);
+            }
+
+            ResultSet rst = stm.executeQuery();
             List<CustomerDTO> customers = new ArrayList<>();
 
             while (rst.next()) {
@@ -38,8 +51,12 @@ public class CustomerServlet extends HttpServlet {
                         rst.getString("address")));
             }
 
+            if (id != null && customers.isEmpty()){
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
 
-            String json = jsonb.toJson(customers);
+            String json = jsonb.toJson(id == null ? customers : customers.get(0));
             resp.setContentType("application/json");
             PrintWriter out = resp.getWriter();
             out.println(json);
@@ -47,6 +64,7 @@ public class CustomerServlet extends HttpServlet {
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
+
     }
 
     @Override
@@ -61,13 +79,13 @@ public class CustomerServlet extends HttpServlet {
 
             CustomerDTO customer = jsonb.fromJson(req.getReader(), CustomerDTO.class);
 
-            if (customer.getId() == null || !customer.getId().matches("C\\d{3}")){
+            if (customer.getId() == null || !customer.getId().matches("C\\d{3}")) {
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Customer id can't be empty");
                 return;
-            }else if (customer.getName() == null || customer.getName().trim().isEmpty()){
+            } else if (customer.getName() == null || customer.getName().trim().isEmpty()) {
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Customer name can't be empty");
                 return;
-            }else if(customer.getAddress() == null || customer.getAddress().trim().isEmpty()){
+            } else if (customer.getAddress() == null || customer.getAddress().trim().isEmpty()) {
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Customer address can't be empty");
                 return;
             }
@@ -86,9 +104,9 @@ public class CustomerServlet extends HttpServlet {
                 throw new RuntimeException("Failed to save the customer, try again");
             }
 
-        }catch (JsonbException exp){
+        } catch (JsonbException exp) {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
-        } catch (SQLException|RuntimeException exp) {
+        } catch (SQLException | RuntimeException exp) {
             throw new RuntimeException(exp);
         }
 
@@ -105,13 +123,13 @@ public class CustomerServlet extends HttpServlet {
         try {
             CustomerDTO customer = jsonb.fromJson(req.getReader(), CustomerDTO.class);
 
-            if (customer.getId() == null || !customer.getId().matches("C\\d{3}")){
+            if (customer.getId() == null || !customer.getId().matches("C\\d{3}")) {
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Customer id can't be empty");
                 return;
-            }else if (customer.getName() == null || customer.getName().trim().isEmpty()){
+            } else if (customer.getName() == null || customer.getName().trim().isEmpty()) {
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Customer name can't be empty");
                 return;
-            }else if(customer.getAddress() == null || customer.getAddress().trim().isEmpty()){
+            } else if (customer.getAddress() == null || customer.getAddress().trim().isEmpty()) {
                 resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Customer address can't be empty");
                 return;
             }
@@ -123,10 +141,10 @@ public class CustomerServlet extends HttpServlet {
                 stm.setString(2, customer.getAddress());
                 stm.setString(3, customer.getId());
 
-                if (stm.executeUpdate() == 1){
+                if (stm.executeUpdate() == 1) {
                     resp.setContentType("application/json");
                     resp.getWriter().println(jsonb.toJson("OK"));
-                }else{
+                } else {
                     throw new RuntimeException("Failed to update the customer");
                 }
 
@@ -134,12 +152,9 @@ public class CustomerServlet extends HttpServlet {
                 throw new RuntimeException(ex);
             }
 
-        }catch (JsonbException ex){
+        } catch (JsonbException ex) {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
         }
-
-
-
     }
 
     @Override
@@ -149,7 +164,7 @@ public class CustomerServlet extends HttpServlet {
 
         String id = req.getParameter("id");
 
-        if (id == null || !id.matches("C\\d{3}")){
+        if (id == null || !id.matches("C\\d{3}")) {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
@@ -159,10 +174,10 @@ public class CustomerServlet extends HttpServlet {
             PreparedStatement stm = connection.prepareStatement("DELETE FROM customer WHERE id=?");
             stm.setString(1, id);
 
-            if (stm.executeUpdate() == 1){
+            if (stm.executeUpdate() == 1) {
                 resp.setContentType("application/json");
                 resp.getWriter().println(jsonb.toJson("OK"));
-            }else{
+            } else {
                 throw new RuntimeException("Failed to delete the customer");
             }
 
