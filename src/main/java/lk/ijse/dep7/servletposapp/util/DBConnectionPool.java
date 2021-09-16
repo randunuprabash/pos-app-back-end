4988e7f312d3d539e778bd65116590907391d459
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-09-16 17:14:58 +0530

    Create necessary connections for the pool

--- src/main/java/lk/ijse/dep7/servletposapp/util/DBConnectionPool.java
+++ src/main/java/lk/ijse/dep7/servletposapp/util/DBConnectionPool.java
@@ -3,19 +3,25 @@ package lk.ijse.dep7.servletposapp.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBConnectionPool {
+
+    private static final int MAX_CONNECTIONS = 4;
+
+    private static List<Connection> connectionPool = new ArrayList<>();
+    private static List<Connection> consumerPool = new ArrayList<>();
 
     static{
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+            for (int i = 0; i < MAX_CONNECTIONS; i++) {
+                connectionPool.add(DriverManager.getConnection("jdbc:mysql://localhost:3306/dep7_backup_pos", "root", "mysql"));
+            }
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/dep7_backup_pos", "root", "mysql");
     }
 
 }
